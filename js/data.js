1c4b632c177953a0cbd0a235281a5b101b493be9
Josh Kuhar <jdkuhar@gmail.com>
2016-11-25 19:30:17 -0500

    added redux store

---
+++ js/data.js
@@ -0,0 +1,28 @@
+var Data = {
+	Wu: {
+		name: 'Wu',
+		phoneNumber: '123-123-1234',
+		address: '123 clan',
+		website: 'http://blahblah',
+		hours: '4pm to 11pm',
+		id: 1
+	},
+	Tang: {
+		name: 'Tang',
+		phoneNumber: '123-321-1234',
+		address: '321 Tang street',
+		website: 'http://tang.com',
+		hours: '12am to 12am',
+		id: 2
+	},
+	Clan: {
+		name: 'Clan',
+		phoneNumber: '321-123-4321',
+		address: '1 Clan street',
+		website: 'http://clan.com',
+		hours: '5pm to 12am',
+		id: 3
+	}
+};
+
+module.exports = Data;
