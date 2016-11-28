c48cf8d7acf2b213bd4608ba957c018258b08e50
Josh Kuhar <jdkuhar@gmail.com>
2016-11-28 18:47:38 -0500

    updated

--- js/actions/index.js
+++ js/actions/index.js
@@ -8,25 +8,34 @@ var findLocations = function(location) {
     }
 };
 
+var ADD_LOCATIONS = 'ADD_LOCATIONS';
+var addLocations = function(locations) {
+	return {
+		type: ADD_LOCATIONS,
+		locations: locations
+	}
+};
 
 
 var SELECT_LOCATION = 'SELECT_LOCATION';
 var selectLocation = function(location) {
     return {
         type: SELECT_LOCATION,
-        location: location,
+        locations: location,
     };
 };
 
 exports.FIND_LOCATIONS = FIND_LOCATIONS;
 exports.findLocations = findLocations;
+exports.ADD_LOCATIONS = ADD_LOCATIONS;
+exports.addLocations = addLocations;
 exports.SELECT_LOCATION = SELECT_LOCATION;
 exports.selectLocation = selectLocation;
 
 var getLocations = function(location){
         if(location == data.Wu.name){
         	console.log(data);
-        	return findLocations(data);
+        	return addLocations(data);
     }
 
 };
